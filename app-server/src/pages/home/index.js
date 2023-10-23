95e1f2b622915d9dbb328140124f78ca999c0357
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-10-23 18:05:20 +0900

    card shine

--- app-server/src/pages/home/index.js
+++ app-server/src/pages/home/index.js
@@ -60,28 +60,28 @@ function Home() {
             </div>
 
             <div
-              className="card dragon / interactive masked"
+              className="card dragon / masked"
               data-number="49"
               data-set="pgo"
-              data-subtypes="basic v"
+              data-subtypes="vmax single strike"
               data-supertype="pokémon"
-              data-rarity="rare holo v"
+              data-rarity="rare rainbow alt"
               data-trainer-gallery="false"
-              //             style="
-              //   --pointer-x: 50%;
-              //   --pointer-y: 50%;
-              //   --pointer-from-center: 0;
-              //   --pointer-from-top: 0.5;
-              //   --pointer-from-left: 0.5;
-              //   --card-opacity: 0;
-              //   --rotate-x: 0deg;
-              //   --rotate-y: 0deg;
-              //   --background-x: 50%;
-              //   --background-y: 50%;
-              //   --card-scale: 1;
-              //   --translate-x: 0px;
-              //   --translate-y: 0px;
-              // "
+              style={{
+                "--pointer-x": "50%",
+                "--pointer-y": "50%",
+                "--pointer-from-center": 0,
+                "--pointer-from-top": "0.5",
+                "--pointer-from-left": "0.5",
+                "--card-opacity": 0,
+                "--rotate-x": "0deg",
+                "--rotate-y": "0deg",
+                "--background-x": "50%",
+                "--background-y": "50%",
+                "--card-scale": "1",
+                "--translate-x": "0px",
+                "--translate-y": "0px",
+              }}
             >
               <div className="card__translater">
                 <button
@@ -99,7 +99,8 @@ function Home() {
                   />{" "} */}
                   <div className="card__front">
                     <img
-                      src="https://images.pokemontcg.io/pgo/49_hires.png"
+                      // src="https://images.pokemontcg.io/pgo/49_hires.png"
+                      src="/assets/images/card_example.png"
                       alt="Front design of the Dragonite V Pokemon Card, with the stats and info around the edge"
                       loading="lazy"
                       width="660"
