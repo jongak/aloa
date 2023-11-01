6ad8b3efe8b4bcbeda3f4844852886ddb15cb818
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-11-01 09:16:41 +0900

    blend canvas에 넣기

--- app-server/src/components/common/CardAvatar.js
+++ app-server/src/components/common/CardAvatar.js
@@ -148,12 +148,7 @@ const CardAvatar = function ({
                 backgroundRepeat: "no-repeat",
                 opacity: 1,
               }}
-            >
-              <img
-                src="/assets/images/card_bg.png"
-                style={{ mixBlendMode: "screen", opacity: "0.4" }}
-              />
-            </div>
+            ></div>
             {/* {[0, 1, 2].map((item) => (
               <div
                 className="imgCrop"
