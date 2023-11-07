4fa411ddaa04f9d7d0fd3546f166d028b2f94b93
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-11-07 11:38:23 +0900

    card front 완 1차

--- app-server/src/components/common/CardFront.js
+++ app-server/src/components/common/CardFront.js
@@ -71,6 +71,43 @@ const CardFront = function ({
     },
   ];
 
+  const change = {
+    기상술사: "aeromancer",
+    아르카나: "arcana",
+    도화가: "artist",
+    바드: "bard",
+    배틀마스터: "battlemaster",
+    버서커: "berserker",
+    블레이드: "blade",
+    블래스터: "blaster",
+    데모닉: "demonic",
+    디스트로이어: "destroyer",
+    데빌헌터: "devilhunter",
+    건슬링어: "gunslinger",
+    호크아이: "hawkeye",
+    홀리나이트: "holyknight",
+    인파이터: "infighter",
+    창술사: "lancemaster",
+    리퍼: "reaper",
+    스카우터: "scouter",
+    슬레이어: "slayer",
+    소서리스: "sorceress",
+    소울이터: "soulEater",
+    기공사: "soulmaster",
+    스트라이커: "striker",
+    서머너: "summoner",
+    워로드: "warlord",
+    암살자: "assassin",
+    "무도가(여)": "fighter_female",
+    "무도가(남)": "fighter-male",
+    "헌터(남)": "hunter_male",
+    "헌터(여)": "hunter-female",
+    마법사: "magician",
+    스페셜리스트: "specialist",
+    "전사(여)": "warrior-female",
+    "전사(남)": "warrior-male",
+  };
+
   useEffect(() => {
     if (characterNameRef.current) {
       getDataCard(characterNameRef.current).then((res) => {
@@ -90,7 +127,6 @@ const CardFront = function ({
       }, 500);
     }
   }, [userData]);
-  // console.log(userData.ArmoryProfile.CharacterName);
   return (
     <div className="cardImg" ref={divRef} style={{ ...style }}>
       <div className="cardBody">
@@ -110,11 +146,11 @@ const CardFront = function ({
               }}
             ></div>
           </div>
-          <div className="engraves" style={{}}>
+          <div className="engraves">
             <ul>
               {userData ? (
-                userData.ArmoryEngraving.fullEffects.map((item) => (
-                  <li key={item.Icon} style={{}}>
+                userData["ArmoryEngraving"]["fullEffects"].map((item) => (
+                  <li key={item.Icon}>
                     <img className="card_front_engraves" src={item.Icon} />
                   </li>
                 ))
@@ -130,12 +166,8 @@ const CardFront = function ({
               )}
               {userData
                 ? userData.ArmoryEngraving.Effects.map((item) => (
-                    <li key={item.Icon} style={{}}>
-                      <img
-                        className="card_front_engraves"
-                        src={item.Icon}
-                        style={{}}
-                      />
+                    <li key={item.Icon}>
+                      <img className="card_front_engraves" src={item.Icon} />
                     </li>
                   ))
                 : ""}
@@ -145,37 +177,42 @@ const CardFront = function ({
             <div className="class_mark">
               <img
                 id="class_mark"
-                src="/assets/images/class_mark/mark-lancemaster-border.png"
-                style={{}}
+                src={
+                  userData
+                    ? `/assets/images/class_mark/mark-${
+                        change[userData["ArmoryProfile"]["CharacterClassName"]]
+                      }-border.png`
+                    : ""
+                }
               />
             </div>
 
             <div className="row justify-content-center">
               <div className="col align-self-center nickname">
-                <h3 style={{}}>
+                <h3>
                   {userData
                     ? userData.ArmoryProfile.CharacterName
                     : "열두자까지가능한닉네임임"}
                 </h3>
               </div>
             </div>
-            <div className="lvl" style={{}}>
-              <div className="card_front_equipments_lvl" style={{}}>
+            <div className="lvl">
+              <div className="card_front_equipments_lvl">
                 <div>
                   <img src="/assets/images/power.png" />
                 </div>
                 <div>
                   {userData
-                    ? userData.ArmoryProfile.ItemAvgLevel.substring(0, 6)
+                    ? userData["ArmoryProfile"]["ItemAvgLevel"].substring(0, 6)
                     : " -"}
                 </div>
                 <div id="card_front_equipments_lvl_decimal">
                   {userData
-                    ? userData.ArmoryProfile.ItemAvgLevel.substring(6, 8)
+                    ? userData["ArmoryProfile"]["ItemAvgLevel"].substring(6, 8)
                     : " "}
                 </div>
               </div>
-              <div className="card_front_expedition_lvl" style={{}}>
+              <div className="card_front_expedition_lvl">
                 <div>
                   <img src="/assets/images/team.png" />
                 </div>
@@ -186,30 +223,53 @@ const CardFront = function ({
             </div>
 
             <div className="container card_front_info">
-              <div
-                className="row justify-content-center card_front_info_table"
-                style={{}}
-              >
+              <div className="row justify-content-center card_front_info_table">
                 <div className="col-2 card_front_info_table_tr">
                   <div className="card_front_info_table_th">세트</div>
                   <div className="card_front_info_table_td">
-                    {userData ? userData.ArmoryEquipment.SetOption : "-"}
+                    {userData &&
+                    userData["ArmoryEquipment"]["option"]["SetOption"]
+                      ? userData.ArmoryEquipment.option.SetOption
+                      : "-"}
                   </div>
                 </div>
                 <div className="col-2 card_front_info_table_tr">
                   <div className="card_front_info_table_th">보석</div>
-                  <div className="card_front_info_table_td">Lv 9.7</div>
+                  <div className="card_front_info_table_td">
+                    <span style={{ letterSpacing: 0 }}>
+                      {userData && userData["ArmoryGem"]["option"]["Level"]
+                        ? "Lv" +
+                          userData["ArmoryGem"]["option"]["Level"].toPrecision(
+                            2
+                          )
+                        : "-"}
+                    </span>
+                    <span style={{ fontSize: "12px", alignSelf: "center" }}>
+                      {/* {userData &&
+                      userData["ArmoryGem"]["option"]["Level"]
+                        ? " " +
+                          userData["ArmoryGem"]["option"]["Level"]
+                            .toPrecision(2)
+                            .substring(1, 3)
+                        : " -"} */}
+                    </span>
+                  </div>
                 </div>
                 <div className="col-2 card_front_info_table_tr">
                   <div className="card_front_info_table_th">엘릭서</div>
                   <div className="card_front_info_table_td">
-                    {userData ? userData.ArmoryEquipment.ElixirName : "-"}
+                    {userData &&
+                    userData["ArmoryEquipment"]["option"]["ElixirName"]
+                      ? userData.ArmoryEquipment.option.ElixirName
+                      : "-"}
                   </div>
                 </div>
                 <div className="col-2 card_front_info_table_tr">
                   <div className="card_front_info_table_th">카드</div>
                   <div className="card_front_info_table_td">
-                    {userData ? userData.ArmoryCard.AwakeName : "-"}
+                    {userData && userData["ArmoryCard"]["AwakeName"]
+                      ? userData.ArmoryCard.AwakeName
+                      : "-"}
                   </div>
                 </div>
                 <div className="col-2 card_front_info_table_tr">
@@ -224,25 +284,61 @@ const CardFront = function ({
                 <div className="col-2 card_front_icons_table_tr">
                   <div>
                     <img
-                      src={userData ? userData.ArmoryEquipment.무기.Icon : ""}
+                      src={
+                        userData
+                          ? userData.ArmoryEquipment.무기.Icon
+                          : "/assets/images/default_slot/weapon.png"
+                      }
                     />
                   </div>
-                  <span className="card_front_icons_table_badge">+25</span>
+                  <span className="card_front_icons_table_badge">
+                    {userData
+                      ? "+" + userData.ArmoryEquipment.무기.ItemGrade
+                      : ""}
+                  </span>
                 </div>
                 <div className="col-2 card_front_icons_table_tr">
                   <div>
-                    <img src="https://cdn-lostark.game.onstove.com/efui_iconatlas/ability/ability_257.png" />
+                    <img
+                      src={
+                        userData &&
+                        userData["ArmoryEquipment"]["어빌리티 스톤"]["Icon"]
+                          ? userData["ArmoryEquipment"]["어빌리티 스톤"]["Icon"]
+                          : "/assets/images/default_slot/stone.png"
+                      }
+                    />
                   </div>
-                  <span className="card_front_icons_table_badge">9</span>
-                  <span className="card_front_icons_table_badge">7</span>
+                  <span className="card_front_icons_table_badge">
+                    {userData &&
+                    userData["ArmoryEquipment"]["어빌리티 스톤"][
+                      "engravings00"
+                    ]["level"]
+                      ? userData["ArmoryEquipment"]["어빌리티 스톤"][
+                          "engravings00"
+                        ]["level"]
+                      : ""}
+                  </span>
+                  <span className="card_front_icons_table_badge">
+                    {userData &&
+                    userData["ArmoryEquipment"]["어빌리티 스톤"][
+                      "engravings01"
+                    ]["level"]
+                      ? userData["ArmoryEquipment"]["어빌리티 스톤"][
+                          "engravings01"
+                        ]["level"]
+                      : ""}
+                  </span>
                 </div>
 
                 <div className="col-2 card_front_icons_table_tr2">
                   <div>
                     <img src="/assets/images/exlixer.webp" />
                   </div>
-                  <span className="card_front_icons_table_badge" style={{}}>
-                    40
+                  <span className="card_front_icons_table_badge">
+                    {userData &&
+                    userData["ArmoryEquipment"]["option"]["ElixirLevel"]
+                      ? userData["ArmoryEquipment"]["option"]["ElixirLevel"]
+                      : "-"}
                   </span>
                 </div>
                 <div className="col-2 card_front_icons_table_tr2">
@@ -252,16 +348,21 @@ const CardFront = function ({
                       src="/assets/images/card.png"
                     />
                   </div>
-                  <span className="card_front_icons_table_badge" style={{}}>
-                    30
+                  <span className="card_front_icons_table_badge">
+                    {userData && userData["ArmoryCard"]["AwakeCount"]
+                      ? userData["ArmoryCard"]["AwakeCount"]
+                      : ""}
                   </span>
                 </div>
                 <div className="col-2 card_front_icons_table_tr2">
                   <div>
                     <img src="/assets/images/cho.png" />
                   </div>
-                  <span className="card_front_icons_table_badge" style={{}}>
-                    40
+                  <span className="card_front_icons_table_badge">
+                    {userData &&
+                    userData["ArmoryEquipment"]["option"]["TransLevel"]
+                      ? userData["ArmoryEquipment"]["option"]["TransLevel"]
+                      : "-"}
                   </span>
                 </div>
               </div>
