52a077a3c1edb430e77830c656fda1e2f17d01b5
kang <kangtu142@gmail.com>
2023-11-08 16:51:43 +0900

    .

--- app-server/src/pages/capture/SelectValue.js
+++ app-server/src/pages/capture/SelectValue.js
@@ -3,10 +3,16 @@ import Button from "../../components/common/Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import styles from "./Main.module.css";
+import ToggleButton from "../../components/common/ToggleButton";
+
 const SelectValue = function () {
   const { setPage, characterNameRef } = useOutletContext();
   const navigate = useNavigate();
   const [userData, setUserData] = useState();
+  const [isName, setIsName] = useState(true);
+  const [isServer, setIsServer] = useState(false);
+
   if (!characterNameRef.current) {
     navigate("../");
   }
@@ -16,6 +22,20 @@ const SelectValue = function () {
   return (
     <div className="option-body">
       <h3>02. 내용 정하기</h3>
+      <div className="userRow">
+        <ToggleButton
+          valueRef={isName}
+          setValueRef={setIsName}
+          title={"닉네임 표시"}
+          body={"닉네임을 숨깁니다."}
+        />
+        <ToggleButton
+          valueRef={isServer}
+          setValueRef={setIsServer}
+          title={"서버 표시"}
+        />
+      </div>
+
       <div>{characterNameRef.current}</div>
       <Button href="../" title={"이전"} />
       <Button href="../set" title={"이후"} />
