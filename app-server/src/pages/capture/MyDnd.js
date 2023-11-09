7f2d3c8e53c1ffb125b877921cb5caa344d98cbe
kang <kangtu142@gmail.com>
2023-11-09 15:58:03 +0900

    ,

---
+++ app-server/src/pages/capture/MyDnd.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+
+const MyDnd = function ({ items, setItems }) {
+  const onDragEnd = (DropResult) => {
+    const { source, destination } = DropResult;
+    if (!destination) return;
+
+    const scourceKey = source.droppableId;
+    const destinationKey = destination.droppableId;
+
+    const _items = JSON.parse(JSON.stringify(items));
+    const [targetItem] = _items[scourceKey].splice(source.index, 1);
+    _items[destinationKey].splice(destination.index, 0, targetItem);
+    setItems(_items);
+  };
+
+  // --- requestAnimationFrame 초기화
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+  // --- requestAnimationFrame 초기화 END
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div
+        className="drag-cover"
+        style={{
+          userSelect: "none",
+          gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
+          borderRadius: "0.5rem",
+        }}
+      >
+        {Object.keys(items).map((key) => (
+          <Droppable key={key} droppableId={key}>
+            {(provided, snapshot) => (
+              <div
+                className={`drag-body ${key} ${
+                  snapshot.isDraggingOver ? "drag" : ""
+                }`}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <div className="drag-title">
+                  {key == "done" ? "활성화" : "비활성화"}
+                </div>
+                {items[key].map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className="drag-item"
+                      >
+                        <div>{item.title}</div>
+                        <span>Make the world beatiful</span>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  );
+};
+export default MyDnd;
--- app-server/src/store/captureSlice.js
+++ app-server/src/store/captureSlice.js
@@ -2,13 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const captureSlice = createSlice({
   name: "review",
-  initialState: { rarityPreset: "custom" },
+  initialState: { rarityPreset: "custom", userData: {} },
   reducers: {
     setRarityPreset(state, action) {
       state.rarityPreset = action.payload.newRarityPreset;
+    },
+    setUserData(state, action) {
+      state.rarityPreset = action.payload.newUserData;
     },
   },
 });
 
 export default captureSlice;
-export const { setRarityPreset } = captureSlice.actions;
+export const { setRarityPreset, setUserData } = captureSlice.actions;
