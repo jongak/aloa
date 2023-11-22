d5481cd05307d6a151437848210dbad0c54c9171
kang <kangtu142@gmail.com>
2023-11-22 11:53:35 +0900

    /

--- api-server/services/character.service.js
+++ api-server/services/character.service.js
@@ -1,24 +1,24 @@
-// const pool = require("../models/pool");
+const pool = require("../models/pool");
 const CharacterModel = require("../models/character.model");
 
 const CharacterService = {
   async getCharacter(characterName) {
-    // const conn = await pool.getConnection();
+    const conn = await pool.getConnection();
     try {
       // 트랜젝션 작업 시작
-      // await conn.beginTransaction();
+      await conn.beginTransaction();
       const data = await CharacterModel.getCharacter(characterName);
       // DB에 작업 반영
-      // await conn.commit();
-      // return { ...data, ok: true };
-      return { ok: true, data: data };
+      await conn.commit();
+      return { ...data, ok: true };
+      // return { ok: true, data: data };
     } catch (err) {
       // DB 작업 취소
-      // await conn.rollback();
+      await conn.rollback();
       throw new Error("Service Error", { cause: err });
     } finally {
       // 커넥션 반납
-      // pool.releaseConnection(conn);
+      pool.releaseConnection(conn);
     }
   },
   async capture(characterName) {
