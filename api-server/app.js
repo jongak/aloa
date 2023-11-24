b4963ca4e524c2237b6dc745eae48c58bc97ead2
kang <kangtu142@gmail.com>
2023-11-24 18:13:35 +0900

    .

--- api-server/app.js
+++ api-server/app.js
@@ -23,50 +23,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "app-server", "build")));
 
 // 교차출처 처리
 // 서버가 다르면 추가
 app.use(cors());
 app.use("/api", indexRouter);
 
-var proxy = require("html2canvas-proxy");
-const request = require("request");
+// var proxy = require("html2canvas-proxy");
+// const request = require("request");
 
-app.use("/api", proxy(), (req, res, next) => {
-  // 프록시 서버 미들웨어
-  switch (req.query.responseType) {
-    case "blob":
-      req.pipe(request(req.query.url).on("error", next)).pipe(res);
-      break;
-    case "text":
-    default:
-      request(
-        { url: req.query.url, encoding: "binary" },
-        (error, response, body) => {
-          if (error) {
-            return next(error);
-          }
-          res.send(
-            `
-            data:${response.headers["content-type"]}; base64, 
-            ${Buffer.from(body, "binary").toString("base64")}`
-          );
-        }
-      );
-      break;
-  }
+// app.use("/api", proxy(), (req, res, next) => {
+//   // 프록시 서버 미들웨어
+//   switch (req.query.responseType) {
+//     case "blob":
+//       req.pipe(request(req.query.url).on("error", next)).pipe(res);
+//       break;
+//     case "text":
+//     default:
+//       request(
+//         { url: req.query.url, encoding: "binary" },
+//         (error, response, body) => {
+//           if (error) {
+//             return next(error);
+//           }
+//           res.send(
+//             `
+//             data:${response.headers["content-type"]}; base64,
+//             ${Buffer.from(body, "binary").toString("base64")}`
+//           );
+//         }
+//       );
+//       break;
+//   }
+// });
+
+// React용 fallback 추가
+app.use("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "app-server", "build", "index.html"));
 });
 
 // 404 에러 처리
 app.use("/api", (req, res, next) => {
   console.error(404, req.url);
   res.json({ error: { message: "404::존재하지 않는 API입니다." } });
-});
-
-app.use(express.static(path.join(__dirname, "..", "app-server", "build")));
-// React용 fallback 추가
-app.use("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "app-server", "build", "index.html"));
 });
 
 // 500 에러 처리
