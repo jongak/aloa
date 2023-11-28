0fbf9e00dc1c540e8890d4a97c83c33694a69b1f
DESKTOP-0QVVBKM\USER <schoug@naver.com>
2023-11-28 09:24:42 +0900

    .

--- app-server/src/pages/capture/SetOption.js
+++ app-server/src/pages/capture/SetOption.js
@@ -73,6 +73,7 @@ const SetOption = function () {
                     className="colorItem"
                     style={{
                       backgroundImage: `url('/assets/images/card_frame/color/${i}.png')`,
+                      backgroundSize: "cover",
                     }}
                     onClick={() => {
                       dispatch(setFrameColor({ newFrameColor: i }));
