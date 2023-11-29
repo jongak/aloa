7c48ceaab4a445a63fbe46afceaca41fb80606ae
kang <kangtu142@gmail.com>
2023-11-29 15:24:11 +0900

    .

--- api-server/routes/users.js
+++ api-server/routes/users.js
@@ -37,4 +37,26 @@ router.get("/file", async (req, res, next) => {
   }
 });
 
+router.get("/img", async (req, res, next) => {
+  const bucketParams = {
+    Bucket: "aloa-bucket",
+    Key: "server_status.png",
+  };
+
+  try {
+    const data = await s3Client.send(new GetObjectCommand(bucketParams));
+    const inputStream = data.Body;
+
+    // Front 이미지를 클라이언트로 스트리밍합니다.
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename='server_status.png'}`
+    );
+    res.setHeader("Content-Type", "image/png"); // 파일 타입에 따라 수정
+    inputStream.pipe(res);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
--- app-server/src/pages/capture/ShareCard.js
+++ app-server/src/pages/capture/ShareCard.js
@@ -3,6 +3,8 @@ import Button from "../../components/common/Button";
 import { useEffect, useRef } from "react";
 import { toast } from "react-toastify";
 import CopyToClipboard from "react-copy-to-clipboard";
+import ToggleButton from "../../components/common/ToggleButton";
+
 const ShareCard = function () {
   const {
     setPage,
@@ -13,15 +15,58 @@ const ShareCard = function () {
   } = useOutletContext();
   const navigate = useNavigate();
 
-  if (!characterNameRef.current) {
-    navigate("../");
-  }
+  const copyLinkRef = useRef({ value: "" });
+  const copyHTMLRef = useRef({ value: "" });
+
+  const frontURL = useRef({ value: "" });
+  const backURL = useRef({ value: "" });
+
+  // if (!characterNameRef.current) {
+  //   navigate("../");
+  // }
   useEffect(() => {
     setPage("share");
   }, []);
 
-  const copyLinkRef = useRef({ value: "" });
-  const copyHTMLRef = useRef({ value: "" });
+  const openPdfInNewTab = () => {
+    const newWindow = window.open(
+      process.env.REACT_APP_API_SERVER + "/user/file",
+      "_blank"
+    );
+    if (newWindow) {
+      newWindow.focus();
+    } else {
+      toast.error(
+        "팝업이 차단되었습니다. 팝업 차단을 해제하고 다시 시도하세요."
+      );
+    }
+  };
+
+  const newHTML = function (front, back) {
+    const frontImgTag = front
+      ? `<img ` +
+        `loading='lazy' ` +
+        `src='${front}' ` +
+        `style='height: 400px; width: 300px; aspect-ratio: 300 / 400' ` +
+        `/> `
+      : "";
+    const backImgTag = back
+      ? `<img ` +
+        `loading='lazy' ` +
+        `src='${back}' ` +
+        `style='height: 400px; width: 300px; aspect-ratio: 300 / 400' ` +
+        `/> `
+      : "";
+    return (
+      `<a href='${process.env.REACT_APP_SERVER}'> ` +
+      frontImgTag +
+      backImgTag +
+      `</a> ` +
+      `<div> ` +
+      `더많은 카드를 만들기 위해서 <a href='${process.env.REACT_APP_SERVER}'>ALOA</a> 방문 ` +
+      `</div> `
+    );
+  };
 
   return (
     <div className="option-body share">
@@ -56,14 +101,16 @@ const ShareCard = function () {
             onChange={() => {}}
           ></input>
           <CopyToClipboard
-            text={copyLinkRef.current.value}
-            onCopy={() => toast.success(`링크를 복사했습니다.`)}
+            text={copyHTMLRef.current.value}
+            onCopy={() => {
+              toast.success(`HTML 태그를 복사했습니다.`);
+            }}
           >
             <Button
               title={
                 <>
                   <i className="fa fa-clipboard" />
-                  &nbsp;&nbsp;클립보드에 복사
+                  &nbsp;&nbsp;HTML 태그 복사
                 </>
               }
             />
@@ -115,11 +162,69 @@ const ShareCard = function () {
           <Button
             title={
               <>
-                <i className="fa fa-cloud-download" />
+                <i className="fa fa-cloud-upload" />
                 &nbsp;&nbsp; 서버에 저장
               </>
             }
             onClick={handleServer}
+          />
+        </div>
+      </div>
+      <h3>
+        <i className="fa fa-server"></i> 서버상태 확인
+      </h3>
+      <div className="userRow">
+        <img
+          src={process.env.REACT_APP_API_SERVER + "/user/img"}
+          style={{ width: "80%" }}
+        />
+        <div className="userbody">
+          위에 사진이 없다면 이미지 서버에 문제가 생긴거에요.
+          <br />
+          아래설명서를 확인하시고 진행해 주세요.
+        </div>
+        <div className="find-input">
+          <input
+            className="form-control"
+            ref={frontURL}
+            placeholder="앞면 이미지 URL"
+          ></input>
+        </div>
+        <div className="find-input">
+          <input
+            className="form-control"
+            ref={backURL}
+            placeholder="뒷면 이미지 URL"
+          ></input>
+        </div>
+        <div className="buttonCover small">
+          <CopyToClipboard
+            text={newHTML(frontURL.current.value, backURL.current.value)}
+            onCopy={() => {
+              if (frontURL.current.value || backURL.current.value) {
+                toast.success(`HTML 태그를 복사했습니다.`);
+              } else {
+                toast.error("URL을 입력해 주세요");
+              }
+            }}
+          >
+            <Button
+              title={
+                <>
+                  <i className="fa fa-clipboard" />
+                  &nbsp;&nbsp;클립보드에 복사
+                </>
+              }
+            />
+          </CopyToClipboard>
+          <Button
+            title={
+              <>
+                <i className="fa fa-file-pdf" />
+                &nbsp;&nbsp; 설명서 보기
+              </>
+            }
+            onClick={openPdfInNewTab}
           />
         </div>
       </div>
