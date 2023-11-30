60c84f07263de10ba94787b63b21605d66166769
kang <kangtu142@gmail.com>
2023-11-30 09:49:16 +0900

    .

--- api-server/myCheck.py
+++ api-server/myCheck.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 def job():
     #우분투
-    # terminal_command = 'netstat -anp | grep -E ":80 |:443 " | grep ESTABLISHED | wc -l'
+    terminal_command = 'netstat -anp | grep -E ":80 |:443 " | grep ESTABLISHED | wc -l'
 
     #윈도우
-    terminal_command = 'netstat -an | findstr ":80 :443" | findstr /C:"ESTABLISHED" /C:"TCP" /C:"127.0.0.1" | find /c /v ""'
+    # terminal_command = 'netstat -an | findstr ":80 :443" | findstr /C:"ESTABLISHED" /C:"TCP" /C:"127.0.0.1" | find /c /v ""'
 
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open('log.txt', 'a', encoding='utf-8') as f:  # 'a'는 파일을 텍스트 모드로 열기 위함
